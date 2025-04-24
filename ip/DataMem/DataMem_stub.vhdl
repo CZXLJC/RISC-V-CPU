918bb30d7453bf139c639ef6c491e3370adbed19
李家成 <163959479+Betterlol@users.noreply.github.com>
2025-04-24 12:56:09 +0800

    add ip core

---
+++ ip/DataMem/DataMem_stub.vhdl
@@ -0,0 +1,41 @@
+-- Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
+-- Date        : Wed Apr 23 10:50:27 2025
+-- Host        : Betterlol running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub -rename_top DataMem -prefix
+--               DataMem_ DataMem_stub.vhdl
+-- Design      : DataMem
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tcsg324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity DataMem is
+  Port ( 
+    clka : in STD_LOGIC;
+    ena : in STD_LOGIC;
+    wea : in STD_LOGIC_VECTOR ( 0 to 0 );
+    addra : in STD_LOGIC_VECTOR ( 13 downto 0 );
+    dina : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    clkb : in STD_LOGIC;
+    enb : in STD_LOGIC;
+    web : in STD_LOGIC_VECTOR ( 0 to 0 );
+    addrb : in STD_LOGIC_VECTOR ( 13 downto 0 );
+    dinb : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    doutb : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end DataMem;
+
+architecture stub of DataMem is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,ena,wea[0:0],addra[13:0],dina[31:0],douta[31:0],clkb,enb,web[0:0],addrb[13:0],dinb[31:0],doutb[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_1,Vivado 2017.4";
+begin
+end;
--- sim/tb_CPU.sv
+++ sim/tb_CPU.sv
@@ -26,14 +26,12 @@
 module tb_CPU;
 
 // CPU Parameters
-parameter PERIOD  = 10;
+parameter PERIOD  = 100;
 
 
 // CPU Inputs
 logic clk                            = 0 ;
 logic rst_n                          = 0 ;
-logic [31:0] instruction             = 0 ;
-logic [13:0] pc                            = 0 ;
 
 // CPU Outputs
 
@@ -51,55 +49,12 @@ end
 
 CPU  u_CPU (
     .clk                 (clk                  ),
-    .rst_n               (rst_n                ),
-    .instruction  (instruction   )
-    // .pc                 (pc                  )
+    .rst_n               (rst_n                )
 );
 
 initial
 begin
-    // Initialize Inputs
-    #(PERIOD*1.5);
-    // Test case 1: Basic instruction
-    // addi x1, x0, 5
-    instruction = 32'b00000000010100000000000010010011; // opcode for addi
-    #PERIOD;
-    // Check outputs
-    // assert (rdata1 == 5) else $error("Test case 1 failed: rdata1 should be 5");
-    // assert (RegWrite == 1) else $error("Test case 1 failed: RegWrite should be 1");
-    // assert (MemWrite == 0) else $error("Test case 1 failed: MemWrite should be 0");
-    // assert (imm32 == 5) else $error("Test case 1 failed: imm32 should be 5");
-    // Test case 2
-    // addi x2, x1, 10
-    instruction = 32'b00000000101000001000000100010011;; // opcode for addi
-    #PERIOD;
-    // Test case 3
-    // add x3, x1, x2
-    instruction = 32'b00000000001000001000000110110011;
-    #PERIOD;
-
-    // Test case 4
-    // sub x4, x1, x2
-    instruction = 32'b01000000001000001000001000110011; // opcode for sub
-    #PERIOD;
-
-    // Test case 5
-    // jal x6, -4
-    instruction = 32'hffdff36f; // opcode for jal
-
-//     // Test case 5： B type instruction  
-
-    // pc = pc + 1;
-    // #PERIOD;
-    // pc = pc + 1;
-    // #PERIOD;
-    // pc = pc + 1;
-    // #PERIOD;
-    // pc = pc + 1;
-    // #PERIOD;
-
-    // #PERIOD;
-    $finish;
+    #(1000) $finish;
 end
 
 endmodule
